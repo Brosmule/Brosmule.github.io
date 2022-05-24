843560bfb50999cd7e565bc6cbaebffe8b7946b9
Brosmule <90448611+Brosmule@users.noreply.github.com>
2022-05-24 11:38:01 +0200

    Buttons actually do something now

--- js/js_githubio.js
+++ js/js_githubio.js
@@ -2,11 +2,11 @@
 
 var clicks = 0;
 function onClick() {
-  if (clicks === 30) {
+  if (clicks === 29) {
     document.getElementById("counter").innerHTML = "Gongratulations you won";
+  } else {
+    clicks += 1;
+    document.getElementById("counter").innerHTML =
+      "You have clicked me " + clicks + " times!";
   }
-
-  clicks += 1;
-  document.getElementById("counter").innerHTML =
-    "You have clicked me " + clicks + " times!";
 }
